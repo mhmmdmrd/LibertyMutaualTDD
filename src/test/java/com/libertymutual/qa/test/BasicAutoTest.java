d1018597878be3ae95f1efafeb43bc009d731816
mhmmdmrd <muradmhmmdit@gmail.com>
2022-08-31 05:03:59 -0400

    Updates

--- src/test/java/com/libertymutual/qa/test/BasicAutoTest.java
+++ src/test/java/com/libertymutual/qa/test/BasicAutoTest.java
@@ -4,7 +4,7 @@ import com.libertymutual.qa.base.BaseClass;
 
 public class BasicAutoTest extends BaseClass{
 
-	
+	//To Do - insert values
 	public void getAutoBasicsQoute() {
 		homePage.homepageSteps1("11208");
 		basicsAutoPage.basicAutoSteps(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
